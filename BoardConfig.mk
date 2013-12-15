3b0813bd4467e436b300c899bd4256014b5ecac8
Schischu <schischu65@gmail.com>
2013-12-15 16:01:08 +0100

    Init

---
+++ BoardConfig.mk
@@ -0,0 +1,153 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+USE_PROPRIETARY_AUDIO_EXTENSIONS := false
+
+# Use a smaller subset of system fonts to keep image size lower
+SMALLER_FONT_FOOTPRINT := true
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := generic
+
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+# DO NOT change the following line to vfpv3 as it is not really supported on our device!
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+
+
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := ap20
+TARGET_BOARD_PLATFORM_GPU := tegra
+TARGET_USERIMAGES_USE_EXT4 := true
+
+
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := androidboot.carrier=wifi-only product_type=w
+KERNEL_MODULES_DIR := /system/lib/modules
+TARGET_KERNEL_SOURCE := device/moto/stingray-kernel
+TARGET_KERNEL_CONFIG := stingray_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+# TARGET_PREBUILT_KERNEL := device/moto/wingray/kernel
+
+# Filesystem
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056858112
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 29859115008
+BOARD_FLASH_BLOCK_SIZE := 4096
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+
+# Camera
+USE_CAMERA_STUB := false
+
+# Graphics
+BOARD_EGL_CFG := device/moto/wingray/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+# BOARD_BLUEDROID_VENDOR_CONF :=
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/wingray/bluetooth
+
+
+
+TARGET_BOARD_INFO_FILE := device/moto/wingray/board-info.txt
+
+
+# Wifi-related defines
+WPA_BUILD_SUPPLICANT        	:= true
+WPA_SUPPLICANT_VERSION      	:= VER_0_8_X
+BOARD_WLAN_DEVICE           	:= bcmdhd
+
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+
+#WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/fsl-tegra-udc/gadget/lun%d/file"
+
+BOARD_HAS_SDCARD_INTERNAL := true
+
+BOARD_UMS_LUNFILE := "/sys/devices/platform/fsl-tegra-udc/gadget/lun%d/file"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TARGET_RECOVERY_UI_LIB := librecovery_ui_stingray
+
+TARGET_RECOVERY_FSTAB = device/moto/wingray/fstab.stingray
+
+# BOARD_USES_SECURE_SERVICES := false
+
+# BOARD_USES_EXTRA_THERMAL_SENSOR := false
+
+# BOARD_LIB_DUMPSTATE := libdumpstate.wingray
+TARGET_RELEASETOOLS_EXTENSIONS := device/moto/wingray
+
+-include vendor/moto/stingray/BoardConfigVendor.mk
+#TWRP config
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_HAS_NO_REAL_SDCARD := true
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+#TARGET_RECOVERY_INITRC := device/moto/wingray/init.recovery.rc
+#TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_FLASH_FROM_STORAGE := true
+TW_NO_USB_STORAGE := true
+
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
---
+++ device.mk
@@ -0,0 +1,149 @@
+#
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file includes all definitions that apply to ALL wingray devices, and
+# are also specific to wingray devices
+#
+# Everything in this directory will become public
+
+DEVICE_PACKAGE_OVERLAYS := device/moto/wingray/overlay
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_PACKAGES := \
+    lights.stingray
+
+PRODUCT_PACKAGES += \
+    sensors.stingray
+
+
+# LOCAL_KERNEL := device/moto/stingray-kernel/kernel
+
+# PRODUCT_COPY_FILES := \
+# 	$(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES := \
+        device/moto/wingray/libaudio/audio_policy.conf:system/etc/audio_policy.conf
+PRODUCT_COPY_FILES += \
+    device/moto/wingray/init.stingray.rc:root/init.stingray.rc \
+    device/moto/wingray/init.stingray.usb.rc:root/init.stingray.usb.rc \
+    device/moto/wingray/fstab.stingray:root/fstab.stingray \
+    device/moto/wingray/ueventd.stingray.rc:root/ueventd.stingray.rc \
+    device/moto/wingray/media_profiles.xml:system/etc/media_profiles.xml \
+    device/moto/wingray/media_codecs.xml:system/etc/media_codecs.xml
+
+# ifneq ($(TARGET_PREBUILT_WIFI_MODULE),)
+# PRODUCT_COPY_FILES += \
+#     $(TARGET_PREBUILT_WIFI_MODULE):system/lib/modules/bcm4329.ko
+# endif
+
+PRODUCT_COPY_FILES += \
+    device/moto/wingray/mXT1386_08_AA.bin:system/etc/firmware/mXT1386_08_AA.bin \
+    device/moto/wingray/mXT1386_08_E1.bin:system/etc/firmware/mXT1386_08_E1.bin \
+    device/moto/wingray/mXT1386_09_AA.bin:system/etc/firmware/mXT1386_09_AA.bin \
+    device/moto/wingray/mXT1386_10_AA.bin:system/etc/firmware/mXT1386_10_AA.bin \
+    device/moto/wingray/mXT1386_10_FF.bin:system/etc/firmware/mXT1386_10_FF.bin
+
+
+
+PRODUCT_COPY_FILES += \
+        device/moto/wingray/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
+        device/moto/wingray/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+        device/moto/wingray/cpcap-key.kcm:system/usr/keychars/cpcap-key.kcm \
+        device/moto/wingray/stingray-keypad.kl:system/usr/keylayout/stingray-keypad.kl \
+        device/moto/wingray/stingray-keypad.kcm:system/usr/keychars/stingray-keypad.kcm
+
+
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+
+    
+# GPS configuration
+PRODUCT_COPY_FILES += \
+	device/moto/wingray/gps.conf:system/etc/gps.conf \
+	device/moto/wingray/gpsconfig.xml:system/etc/gpsconfig.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	e2fsck
+
+PRODUCT_PACKAGES += \
+    audio_policy.stingray \
+    audio.primary.stingray \
+    audio.a2dp.default \
+    audio.usb.default \
+
+PRODUCT_PACKAGES += \
+    l2ping \
+    hcitool \
+    bttest \
+    whisperd
+
+
+
+
+# Bluetooth config file
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf \
+
+WIFI_BAND := 802_11_ABG
+
+PRODUCT_PACKAGES += \
+    camera.stingray
+
+PRODUCT_PACKAGES += \
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product-if-exists, vendor/moto/stingray/wingray-vendor.mk)
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
